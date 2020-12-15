c96fce0e445a06f4c486637e7a1ef6e564d42b28
rml60 <r.m-l@gmx.de>
2020-12-15 09:21:38 +0100

    Umgang mit cursor-Positionen ueberarbeitet.(printStr, move)

--- lcd/lcdhd44780.py
+++ lcd/lcdhd44780.py
@@ -1,50 +1,45 @@
 """ Provides an API for talking to HD44780 compatible character LCDs.
 """
-
 import time
 
 class LcdHD44780:
   """ Implements the API for talking with HD44780 compatible character LCDs.
-      This class only knows what commands to send to the LCD, and not how to get
-      them to the LCD.
-      It is expected that a derived class will implement the hal_xxx functions.
+      This class only knows what commands to send to the LCD, and not
+      how to get them to the LCD.
+      It is expected that a derived class will implement the dummy functions.
   """
-  # The following constant names were lifted from the avrlib lcd.h
-  # header file, however, I changed the definitions from bit numbers
-  # to bit masks.
-  #
-  # HD44780 LCD controller command set
+  # HD44780 LCD controller command set (avrlib lcd.h header file)
 
-  LCD_CLR = 0x01              # DB0: clear display
-  LCD_HOME = 0x02             # DB1: return to home position
+  LCD_CLR        = 0b00000001     # 0x01 DB0: clear display
+  LCD_HOME       = 0b00000010     # 0x02 DB1: return to home position
 
-  LCD_ENTRY_MODE = 0x04       # DB2: set entry mode
-  LCD_ENTRY_INC = 0x02        # --DB1: increment
-  LCD_ENTRY_SHIFT = 0x01      # --DB0: shift
+  LCD_ENTRY_MODE = 0b00000100     # 0x04 DB2: set entry mode
+  LCD_ENTRY_INC  = 0b00000010     # 0x02 --DB1: increment
+  LCD_ENTRY_SHIFT= 0b00000001     # 0x01 --DB0: shift
 
-  LCD_ON_CTRL = 0x08          # DB3: turn lcd/cursor on
-  LCD_ON_DISPLAY = 0x04       # --DB2: turn display on
-  LCD_ON_CURSOR = 0x02        # --DB1: turn cursor on
-  LCD_ON_BLINK = 0x01         # --DB0: blinking cursor
+  LCD_ON_CTRL    = 0b00001000     # 0x08 DB3: turn lcd/cursor on
+  LCD_ON_DISPLAY = 0b00000100     # 0x04 --DB2: turn display on
+  LCD_ON_CURSOR  = 0b00000010     # 0x02 --DB1: turn cursor on
+  LCD_ON_BLINK   = 0b00000010     # 0x01 --DB0: blinking cursor
 
-  LCD_MOVE = 0x10             # DB4: move cursor/display
-  LCD_MOVE_DISP = 0x08        # --DB3: move display (0-> move cursor)
-  LCD_MOVE_RIGHT = 0x04       # --DB2: move right (0-> left)
+  LCD_MOVE       = 0b00010000     # 0x10 DB4: move cursor/display
+  LCD_MOVE_DISP  = 0b00001000     # 0x08 --DB3: move display (0-> move cursor)
+  LCD_MOVE_RIGHT = 0b00000100     # 0x04 --DB2: move right (0-> left)
 
-  LCD_FUNCTION = 0x20         # DB5: function set
-  LCD_FUNCTION_8BIT = 0x10    # --DB4: set 8BIT mode (0->4BIT mode)
-  LCD_FUNCTION_2LINES = 0x08  # --DB3: two lines (0->one line)
-  LCD_FUNCTION_10DOTS = 0x04  # --DB2: 5x10 font (0->5x7 font)
-  LCD_FUNCTION_RESET = 0x30   # See "Initializing by Instruction" section
+  LCD_FUNCTION       = 0b00100000 # 0x20 DB5: function set
+  LCD_FUNCTION_8BIT  = 0b00010000 # 0x10 --DB4: set 8BIT mode (0->4BIT mode)
+  LCD_FUNCTION_2LINES= 0b00001000 # 0x08 --DB3: two lines (0->one line)
+  LCD_FUNCTION_10DOTS= 0b00000100 # 0x04 --DB2: 5x10 font (0->5x7 font)
+  LCD_FUNCTION_RESET = 0b00110000 # 0x30 See "Initializing by Instruction" section
 
-  LCD_CGRAM = 0x40            # DB6: set CG RAM address
-  LCD_DDRAM = 0x80            # DB7: set DD RAM address
+  LCD_CGRAM      = 0b01000000     # 0x40 DB6: set CG RAM address
+  LCD_DDRAM      = 0b10000000     # 0x80 DB7: set DD RAM address
 
-  LCD_RS_CMD = 0
-  LCD_RS_DATA = 1
+  LCD_RS_CMD   = 0
+  LCD_RS_DATA  = 1
 
   LCD_RW_WRITE = 0
-  LCD_RW_READ = 1
+  LCD_RW_READ  = 1
 
   def __init__(self, num_lines, num_columns):
     self.num_lines = num_lines
@@ -79,8 +74,8 @@ class LcdHD44780:
     """
     self.__writeCommand(self.LCD_CLR)
     self.__writeCommand(self.LCD_HOME)
-    self.__setCol(0)
-    self.__setRow(0)
+    self.__row = 0
+    self.__col = 0
 
   def cursorOn(self):
     """ Causes the cursor to be made visible.
@@ -130,12 +125,16 @@ class LcdHD44780:
     self.__backlight = False
     self.__backlightOff()
 
-  def move(self, row, col):
+  def move(self, row, col, setRecent=False):
     """ Moves the cursor position to the indicated position. The cursor
         position is zero based (i.e. cursor_x == 0 indicates first column).
+        Default behavior is without saving the recent cursor position.
     """
-    self.__setCol(col)
-    self.__setRow(row)
+    if setRecent:
+      self.__rowRecent = self.__row
+      self.__colRecent = self.__col
+    self.__row = row
+    self.__col = col
     addr = col & 0x3f
     if row & 1:
       addr += 0x40    # Lines 1 & 3 add 0x40
@@ -143,28 +142,38 @@ class LcdHD44780:
       addr += 0x14    # Lines 2 & 3 add 0x14
     self.__writeCommand(self.LCD_DDRAM | addr)
 
-  def putChar(self, char):
+  def printChar(self, char):
     """ Writes the indicated character to the LCD at the current cursor
         position, and advances the cursor by one position.
     """
     if char != '\n':
       self.__writeData(ord(char))
-      self.__setCol(self.__col + 1)
+      self.__col += 1
     if self.__col >= self.num_columns or char == '\n':
-      self.__setCol(0)
-      self.__setRow(self.__row + 1)
+      self.__col = 0
+      self.__row += 1
       if self.__row >= self.num_lines:
-        self.__setRow(0)
+        self.__row = 0
       self.move(self.__row, self.__col)
 
-  def print(self, string):
-    """ Write the indicated string to the LCD at the current cursor
-        position and advances the cursor position appropriately.
+  def printStr(self, string, row=None, col=None):
+    """ writes the specified string either
+          at the current position and set the cursor behind the string
+        or
+          at the specified coordinates and set the cursor at the recent
+          position.
     """
+    self.__rowRecent = self.__row
+    self.__colRecent = self.__col
+    if row is not None and col is not None:
+      self.move(row, col)
     for char in string:
-      self.putChar(char)
+      self.printChar(char)
+    if row is not None and col is not None:
+      self.move(self.__rowRecent, self.__colRecent)
+    
 
-  def custom_char(self, location, charmap):
+  def customChar(self, location, charmap):
     """ Write a character to one of the 8 CGRAM locations, available
         as chr(0) through chr(7).
     """
@@ -177,28 +186,26 @@ class LcdHD44780:
     self.move(self.__row, self.__col)
 
   def __backlightOn(self):
-    """ Allows the hal layer to turn the backlight on.
-        If desired, a derived HAL class will implement this function.
+    """ Dummy: Allows the hal layer to turn the backlight on.
+        If desired, a derived class will implement this function.
     """
     pass
 
   def __backlightOff(self):
-    """  Allows the hal layer to turn the backlight off.
-         If desired, a derived HAL class will implement this function.
+    """  Dummy: Allows the hal layer to turn the backlight off.
+         If desired, a derived class will implement this function.
     """
     pass
 
   def __writeCommand(self, cmd):
-    """ Write a command to the LCD.
-        It is expected that a derived HAL class will implement this
-        function.
+    """ Dummy: Write a command to the LCD.
+        It is expected that a derived class will implement this function.
     """
     raise NotImplementedError
 
   def __writeData(self, data):
-    """ Write data to the LCD.
-        It is expected that a derived HAL class will implement this
-        function.
+    """ Dummy: Write data to the LCD.
+        It is expected that a derived class will implement this function.
     """
     raise NotImplementedError
 
@@ -207,12 +214,3 @@ class LcdHD44780:
     """
     time.sleep_us(usecs)
     
-  def __setCol(self, col):
-    """
-    """
-    self.__colRecent = self.__col
-    self.__col = col
-    
-  def __setRow(self, row):
-    self.__rowRecent = self.__row
-    self.__row = row
